be57cf1a805b8f4a3865ddba6d67cc1ff78227a8
imagi <9677226@mail.ru>
2022-07-12 12:45:54 +0600

    registr add

--- src/test/java/ru/mundaworld/FirstTest.java
+++ src/test/java/ru/mundaworld/FirstTest.java
@@ -2,12 +2,6 @@ package ru.mundaworld;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -54,6 +48,14 @@ public class FirstTest extends WebDriverSettings {
     @Test
     public void testRegistr() throws InterruptedException {
 
+        String email = "96775asassdsd55@mail.ruw";
+        String phone = "79978585544";
+        String name  = "Azat";
+        String surname = "Test";
+        String adress = "Markova 75";
+        String postindex = "050000";
+        String password = "Qwerty123@";
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://mundaworld.com/registration");
 
@@ -62,18 +64,18 @@ public class FirstTest extends WebDriverSettings {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 
-        driver.findElement(By.xpath("//input[contains(@type, 'tel')]")).sendKeys("7052418070");
+        driver.findElement(By.xpath("//input[contains(@type, 'tel')]")).sendKeys(phone);
         driver.findElement(By.xpath("//button[@class = 'button -orange button-position mt-48']")).click();
 
 
 
         //WebElement input = driver.findElement(By.cssSelector());
 
-        driver.findElement(By.xpath("//input[contains(@type, 'text')]")).sendKeys("Azat");
+        driver.findElement(By.xpath("//input[contains(@type, 'text')]")).sendKeys(name);
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//span[contains(text(), 'Фамилия')]/following-sibling::input")).sendKeys("Kadyr");
+        driver.findElement(By.xpath("//span[contains(text(), 'Фамилия')]/following-sibling::input")).sendKeys(surname);
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//input[contains(@type, 'email')]")).sendKeys("9677226@mail.rur");
+        driver.findElement(By.xpath("//input[contains(@type, 'email')]")).sendKeys(email);
         Thread.sleep(1000);
         driver.findElement(By.xpath("//input[contains(@type, 'checkbox')]")).click();
         Thread.sleep(1000);
@@ -89,20 +91,27 @@ public class FirstTest extends WebDriverSettings {
         Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[2]/div/div[3]/ul/li[10]/span")).click();
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//span[contains(text(), 'Адрес*')]/following-sibling::input")).sendKeys("Тестовый адрес");
+        driver.findElement(By.xpath("//span[contains(text(), 'Адрес*')]/following-sibling::input")).sendKeys(adress);
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//span[contains(text(), 'Почтовый индекс')]/following-sibling::input")).sendKeys("050000");
+        driver.findElement(By.xpath("//span[contains(text(), 'Почтовый индекс')]/following-sibling::input")).sendKeys(postindex);
         Thread.sleep(1000);
         driver.findElement(By.xpath("//button[@class = 'button -orange button-position mt-48']")).click();
 
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[1]/div/input")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[1]/div/input")).sendKeys(password);
         driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[1]/div/div")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[2]/div/input")).sendKeys("123456");
+        driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[2]/div/input")).sendKeys(password);
         driver.findElement(By.xpath("//*[@id=\"authLayout\"]/div/div/div/div[2]/div[4]/div[2]/div/div")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//button[@class = 'button -orange button-position mt-48']")).click();
+
+        String ele = driver.getTitle();
+        System.out.println(ele);
+
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"defaultLayout\"]/div[3]/div[3]/div[2]/div/a[1]/div/div[1]")).click();
+
 
 
  }
